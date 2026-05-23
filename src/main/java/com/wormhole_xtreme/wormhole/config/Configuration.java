49f64e6abd33003f7e425a7766aa18dbe1eca337
Justin <khanjal@gmail.com>
2026-05-23 14:40:37 -0400

    Convert file headers to SPDX short form
    Replace long GPLv3 comment blocks with concise SPDX license identifiers and a short copyright line across source files. Also remove a few unused imports and stray/commented TODOs (e.g. Configuration.java imports, WXList/Stargate3DShape/StargateManager comments) and perform minor cleanup to modernize headers and reduce file noise. Affects multiple Java source files under com.wormhole_xtreme (commands, config, model, listeners, logic, etc.).

--- src/main/java/com/wormhole_xtreme/wormhole/config/Configuration.java
+++ src/main/java/com/wormhole_xtreme/wormhole/config/Configuration.java
@@ -1,29 +1,10 @@
-/*
- *   Wormhole X-Treme Plugin for Bukkit
- *   Copyright (C) 2011  Ben Echols
- *                       Dean Bailey
- *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2011 Ben Echols, Dean Bailey. See LICENSE.txt for terms.
 package com.wormhole_xtreme.wormhole.config;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Set;
@@ -33,7 +14,6 @@ import org.bukkit.plugin.PluginDescriptionFile;
 
 import com.wormhole_xtreme.wormhole.WormholeXTreme;
 import com.wormhole_xtreme.wormhole.config.ConfigManager.ConfigKeys;
-import com.wormhole_xtreme.wormhole.permissions.PermissionsManager.PermissionLevel;
 
 /**
  * The Class Configuration.
