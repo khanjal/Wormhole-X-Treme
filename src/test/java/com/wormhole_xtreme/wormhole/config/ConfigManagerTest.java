5cf1fe4c307d187459c5d3100a70b4e6d5a5ddf8
Justin <khanjal@gmail.com>
2026-05-25 23:25:26 -0400

    Update ConfigManagerTest.java

--- src/test/java/com/wormhole_xtreme/wormhole/config/ConfigManagerTest.java
+++ src/test/java/com/wormhole_xtreme/wormhole/config/ConfigManagerTest.java
@@ -3,10 +3,27 @@ package com.wormhole_xtreme.wormhole.config;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ConfigManagerTest
 {
+    @BeforeEach
+    public void setUp()
+    {
+        try
+        {
+            final java.lang.reflect.Field f = ConfigManager.class.getDeclaredField("configurations");
+            f.setAccessible(true);
+            @SuppressWarnings("unchecked")
+            final java.util.concurrent.ConcurrentHashMap<ConfigManager.ConfigKeys, Setting> map = (java.util.concurrent.ConcurrentHashMap<ConfigManager.ConfigKeys, Setting>) f.get(null);
+            map.clear();
+        }
+        catch (final Exception e)
+        {
+            // ignore
+        }
+    }
     @AfterEach
     public void tearDown()
     {
